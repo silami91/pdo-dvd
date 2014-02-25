aa56d255ddaf445842fc54148294d9e057c953d9
Steven Ilami <ilami.steven@gmail.com>
2014-02-24 20:20:27 -0800

    Deleting a bunch of stuff

--- pdo-dvd/PHP-Auth-A3/ITP/Auth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace ITP\Auth
-
-class Auth{
-
-}
--- pdo-dvd/PHP-Auth-A3/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-$session = new Session();
-$session->start();
-$session->destroy();
-
-?>
